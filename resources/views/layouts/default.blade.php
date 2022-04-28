0826e1728ca2100e030e4f641776ed164289088d
jinhesui <jinhesui@163.com>
2022-04-28 18:16:15 +0800

    静态文件浏览器缓存问题

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
   <head>
     <title>@yield('title', 'Weibo App') - 谁人的日志</title>
-    <link rel="stylesheet" href="/css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
 
@@ -15,7 +15,7 @@
         </ul>
       </div>
     </nav>
-    
+
     <div class="container">
       @yield('content')
     </div>
