9b5d2b66442aab6120d964b4a3eafb54fee72551
jinhesui <jinhesui@163.com>
2022-05-07 15:08:39 +0800

    管理员可删除用户

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -15,6 +15,7 @@ class UsersTableSeeder extends Seeder
         $user = User::find(1);
         $user->name = 'jinhesui';
         $user->email = 'jinhesui@example.com';
+        $user->is_admin = true;
         $user->save();
     }
 }
